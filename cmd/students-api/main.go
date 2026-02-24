9d0cfdcc6957934d49a57061c4a53658aa5c4caf
Prohor722 <prohor.b722@gmail.com>
2026-02-25 01:50:35 +0600

    update change

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/Prohor722/go_rest_api_1/internal/config"
+)
+
+// import "fmt"
 
 func main() {
-	fmt.Println("Welcome to students api!")
+	// load config
+	cfg := config.MustLoad()
+
+
+	// database setup
+	// setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("welcome to students api..."))
+	})
+
+
+	// setup server
+	server := http.Server {
+		Addr: cfg.Address,
+		Handler: router,
+	}
+
+
+	fmt.Println("Server Started!!")
+	fmt.Printf("Serving on: %s",cfg.Address)
+
+	err := server.ListenAndServe()
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func(){
+		if err != nil {
+			log.Fatal("Fail to start server !")
+		}
+	}()
+
+	<-done
+
+	slog.Info("shutting down the server..")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+
+	defer cancel()
+
+	e := server.Shutdown(ctx)
+
+	if e != nil {
+		slog.Error("Failed to shutdown server", slog.String("error:",e.Error()))
+	}
+
+	slog.Info("Server shutdown successfully !")
+
 }
