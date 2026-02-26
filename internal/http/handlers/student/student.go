c6580a91ec6ec4f275e726dda5df2a9c43746b42
Prohor722 <prohor.b722@gmail.com>
2026-02-26 23:45:32 +0600

    creating student on process

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -1,12 +1,18 @@
 package student
 
 import (
+	"encoding/json"
 	"log/slog"
 	"net/http"
+
+	"github.com/Prohor722/go_rest_api_1/internal/types"
 )
 
 func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request){
+		var student types.Student
+		json.NewDecoder(r.Body).Decode(&student)
+		
 		slog.Info("creating a student")
 		w.Write([]byte("welcome to students api..."))
 	}
