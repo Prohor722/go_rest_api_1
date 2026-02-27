7dc2883d13d57715ccbe31022534a0cdc8774ee1
Prohor722 <prohor.b722@gmail.com>
2026-02-27 21:52:13 +0600

    creating student on process

--- internal/utils/response/response.go
+++ internal/utils/response/response.go
@@ -6,8 +6,14 @@ import (
 )
 
 type Response struct{
-	
+	Status string
+	Error string
 }
+
+const (
+	Status = "OK"
+	StatusError = "Error"
+)
 
 func WriteJson(w http.ResponseWriter, status int, data interface {}) error {
 	w.Header().Set("Content-Type","application/json")
@@ -17,5 +23,7 @@ func WriteJson(w http.ResponseWriter, status int, data interface {}) error {
 }
 
 func GeneralError(err error) Response {
-
+	return Response{
+		Status: ,
+	}
 }
