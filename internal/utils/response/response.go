de8dadb0af92944525244c392c68e71d26ba18bb
Prohor722 <prohor.b722@gmail.com>
2026-02-27 22:19:34 +0600

    request validation on creating student on process

--- internal/utils/response/response.go
+++ internal/utils/response/response.go
@@ -2,7 +2,10 @@ package response
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"github.com/go-playground/validator"
 )
 
 type Response struct{
@@ -29,4 +32,20 @@ func GeneralError(err error) Response {
 	}
 }
 
-func 
+func validationError(errs validator.ValidationErrors) Response {
+	var errMsgs []string
+
+	for _,err:=range errs{
+		switch err.ActualTag() {
+		case "required":
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s is required.", err.Field()))
+		default:
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s is invalid.", err.Field()))
+		}
+	}
+
+	
+	return Response{
+
+	}
+}
