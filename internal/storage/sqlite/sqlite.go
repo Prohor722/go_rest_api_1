5856bf0f7afd03db3687d38bd42953bd08f97de9
Prohor722 <prohor.b722@gmail.com>
2026-02-28 21:10:01 +0600

    db setup on process

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -1,2 +1,18 @@
 package sqlite
 
+import (
+	"database/sql"
+
+	"github.com/Prohor722/go_rest_api_1/internal/config"
+)
+
+type Sqlite struct {
+	Db *sql.DB
+}
+
+func New(cfg *config.Config) (*Sqlite, error) {
+	_,err := sql.Open("sqlite3", cfg.StoragePath)
+	if err != nil {
+		return nil, err
+	}
+}
