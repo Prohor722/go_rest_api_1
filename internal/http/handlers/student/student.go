dc50f7b49dbacd649ff92d0d5d23b39d4375b5b3
Prohor722 <prohor.b722@gmail.com>
2026-03-02 00:34:07 +0600

    get student api on done

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
+	"strconv"
 
 	"github.com/Prohor722/go_rest_api_1/internal/storage"
 	"github.com/Prohor722/go_rest_api_1/internal/types"
@@ -64,6 +65,20 @@ func GetById(storage storage.Storage) http.HandlerFunc {
 		id := r.PathValue("id")
 		slog.Info("Getting students data....", slog.String("id",id))
 
-		
+		intId, err := strconv.ParseInt(id, 10, 64)
+
+		if err != nil {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+		student, err := storage.GetStudentById(intId)
+
+		if err != nil {
+			slog.Error("Error getting user", slog.String("id", id))
+			response.WriteJson(w, http.StatusInternalServerError, response.GeneralError(err))
+			return
+		}
+
+		response.WriteJson(w, http.StatusOK, student)
 	}
 }
